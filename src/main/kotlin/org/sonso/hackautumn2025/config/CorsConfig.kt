de04224b49a8ede9ba1b1b0212f0c21df5714e5f
Ivan "Kinok0" Zapara <69762652+Kinok0Bash@users.noreply.github.com>
2025-10-25 12:24:24 +0300

    Попытка поправить корсы

--- src/main/kotlin/org/sonso/hackautumn2025/config/CorsConfig.kt
+++ src/main/kotlin/org/sonso/hackautumn2025/config/CorsConfig.kt
@@ -14,7 +14,7 @@ class CorsConfig {
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
         configuration.applyPermitDefaultValues()
-        configuration.allowedOrigins = listOf("*")
+        configuration.allowedOriginPatterns = listOf("*")
         configuration.allowedMethods = listOf("*")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
